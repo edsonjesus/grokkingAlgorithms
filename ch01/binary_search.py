0d559da1618961db4f44edb81990e4a802805af6
Edson Jesus <edsonsjesus@gmail.com>
2020-11-20 12:18:18 -0300

    Correction of the algorithm. I think there is an error in the book.

--- ch01/binary_search.py
+++ ch01/binary_search.py
@@ -3,7 +3,7 @@ def binary_search(my_list, item):
     high = len(my_list) - 1
 
     while low <= high:
-        mid = low + high
+        mid = round((low + high)/2)
         guess = my_list[mid]
         if guess == item:
             return mid
@@ -14,6 +14,5 @@ def binary_search(my_list, item):
     return None
 
 
-my_list = [1, 3, 5, 7, 9]
-print(binary_search(my_list, 3))
-print(binary_search(my_list, -1))
+my_list = [1, 3, 5, 7, 9, 10, 12]
+print(binary_search(my_list, 10))
